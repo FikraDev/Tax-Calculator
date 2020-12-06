c988abd658bc43216905a7629bfd7d9b829a750b
fikradev <sachel.campbell@gmail.com>
2020-12-06 16:40:11 -0500

    Second Commit

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ let result = document.getElementById("txtArea");
 let initValue = document.getElementById("itemValue");
 
 calcBtn.onclick  = function calcAllTaxes(){
-    if(tType.value=="gct-Comp"){
+    if(tType.value=="gct-Comp" || tType.value=="gct-Gen"){
     calculation = (0.15*initValue.value) + Number(initValue.value);
     result.innerHTML=("$" + calculation.toFixed(2))
     event.preventDefault();
   }
 
-    else{
-        alert('Wrong Value')
+    else if (tType.value=="gct-Tele"){
+        calculation = (0.25*initValue.value) + Number(initValue.value);
+        result.innerHTML=("$" + calculation.toFixed(2))
+        event.preventDefault();
     }
 }
 
