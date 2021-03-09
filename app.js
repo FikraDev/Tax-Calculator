9424c750f139c288e8a0ce827ff4a10cd7281757
fikradev <sachel.campbell@gmail.com>
2021-03-08 19:46:13 -0500

    input functionality added

--- app.js
+++ app.js
@@ -7,20 +7,31 @@ const taxType = document.querySelector('.taxType');
 
 const solution = document.querySelector('.solution');
 
+const error = document.querySelector('.errmsg');
+
+const error2= document.querySelector('.errmsgx');
+
 function checkVal(){
     if (form.valinput.value===''){
-        form.valinput.setAttribute('class', 'error');
+        error.classList.toggle('errmsg2');    
     }
 }
 
 function calcTaxes(){
     if (taxType.value==='gct-cons'){
-        solution.innerHTML=(form.valinput.value * .15).toFixed(2)
-        
+        solution.innerHTML=`$ ${(form.valinput.value * .15).toFixed(2)}`;        
+    }
+    else if(taxType.value==="gct-tel"){
+        solution.innerHTML=`$ ${(form.valinput.value * .25).toFixed(2)}`; 
+    }
+    else if(taxType.value==='make-selection' || taxType.value==''){
+        error2.classList.toggle('errmsg2x');
+
     }
 }
 
 calcBtn.addEventListener('click', ()=>{
+    
     checkVal();
     calcTaxes();
 })
