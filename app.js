40ded302cdec024eaf5ad9d060372a28b8614052
fikradev <sachel.campbell@gmail.com>
2021-03-08 20:03:40 -0500

    fixed responsiveness

--- app.js
+++ app.js
@@ -9,29 +9,36 @@ const solution = document.querySelector('.solution');
 
 const error = document.querySelector('.errmsg');
 
-const error2= document.querySelector('.errmsgx');
+const error2 = document.querySelector('.errmsgx');
 
-function checkVal(){
-    if (form.valinput.value===''){
-        error.classList.toggle('errmsg2');    
+function checkVal() {
+    if (form.valinput.value === '') {
+        error.classList.toggle('errmsg2');
     }
 }
 
-function calcTaxes(){
-    if (taxType.value==='gct-cons'){
-        solution.innerHTML=`$ ${(form.valinput.value * .15).toFixed(2)}`;        
+function calcTaxes() {
+    if (taxType.value === 'gct-cons') {
+        solution.innerHTML = `$ ${(form.valinput.value * .15).toFixed(2)}`;
     }
-    else if(taxType.value==="gct-tel"){
-        solution.innerHTML=`$ ${(form.valinput.value * .25).toFixed(2)}`; 
+    else if (taxType.value === "gct-tel") {
+        solution.innerHTML = `$ ${(form.valinput.value * .25).toFixed(2)}`;
     }
-    else if(taxType.value==='make-selection' || taxType.value==''){
+    else if (taxType.value === 'make-selection' || taxType.value == '') {
         error2.classList.toggle('errmsg2x');
 
     }
 }
 
-calcBtn.addEventListener('click', ()=>{
-    
+function clearInputs() {
+    window.location.reload(); /*reloads page*/
+}
+
+calcBtn.addEventListener('click', () => {
     checkVal();
     calcTaxes();
+});
+
+clrBtn.addEventListener('click', () => {
+    clearInputs();
 })
